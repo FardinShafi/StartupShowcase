64783e6b26cabf04ca294e52e85622b08a43c984
NITRO\fardin <fardin467@gmail.com>
2023-09-03 22:08:20 +0600

    offergarage done

--- login.js
+++ login.js
@@ -1,35 +1,24 @@
 function load() {
     var selectedOption = document.getElementById("menuX").value;
     var urlToLoad = "";
-    var number = Math.random();
+    
 
-    if (selectedOption === "1" && number < 0.7) {
-        urlToLoad = "bookGarage.html";
+    if (selectedOption === "1" ) {
+    urlToLoad = "bookGarage.html";
     }
-    else if (selectedOption === "1" && number >= 0.7 && number < 0.85) {
-        urlToLoad = "processing.html";
-    }
-    else if (selectedOption === "1" && number >= 0.85) {
-        urlToLoad = "ban.html";
-    } 
-    else if (selectedOption === "2" && number < 0.7) {
+  
+    else if (selectedOption === "2" ) {
         urlToLoad = "offerGarage.html";
     }
-    else if (selectedOption === "2" && number >= 0.7 && number < 0.85) {
-        urlToLoad = "processing.html";
-    }
-    else if (selectedOption === "2" && number >= 0.85) {
-        urlToLoad = "ban.html";
-    } 
-    else if (selectedOption === "3" && number < 0.7) {
+   
+    else if (selectedOption === "3") {
         urlToLoad = "supervise.html";
     }
-    else if (selectedOption === "3" && number >= 0.7 && number < 0.85) {
-        urlToLoad = "processing.html";
+
+    else if (selectedOption === "4") {
+        urlToLoad = "admin.html";
     }
-    else if (selectedOption === "3" && number >= 0.85) {
-        urlToLoad = "ban.html";
-    } 
+    
 
     if (urlToLoad !== "") {
         window.location.href = urlToLoad;
